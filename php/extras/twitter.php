c6bca352bee6bfabc666df46eee4562ec9aab939
Steven Lloyd <steve.lloyd@gmail.com>
2015-02-28 21:14:11 -0700

    added twitterTweets function to scrape last tweets of a twitter account

--- php/extras/twitter.php
+++ php/extras/twitter.php
@@ -3,6 +3,51 @@
 	http://apiwiki.twitter.com/Twitter-API-Documentation
 */
 $progpath=dirname(__FILE__);
+//---------- begin function twitterTweets-------------------
+/**
+* @describe scrapes tweets from a twitter url - no auth required
+* @param url string
+* @param cnt integer  number of tweets to scrape
+* @param hrs integer  number of hours to cache.  Set to 0 to disable cache. Defaults to 3 hours
+* @return array
+* @usage $tweets=twitterTweets('https://twitter.com/@myurl',3);
+*/
+function twitterTweets($url,$cnt=3,$cache=3) {
+	$page=getStoredValue("return twitterFetch('{$url}');",0,$cache);
+	if(!strlen($page)) {
+		return '';
+	}
+	preg_match_all('/\<p class="(.+?)\"(.+?)\>(.+?)\<\/p\>/ism',$page,$ptags,PREG_SET_ORDER);
+	$tweets=array();
+	foreach($ptags as $ptag){
+        $htm=$ptag[0];
+        if(!preg_match('/tweet/',$ptag[1])){continue;}
+        //echo printValue($ptag);
+        $txt=preg_replace('/\<a(.+?)\<\/a\>/','',$htm);
+		$tag=array('txt'=>strip_tags($txt));
+        if(preg_match('/a href\=\"(http|https)(\:\/\/)(.+?)\"/',$htm,$m)){
+			$tag['url']=$m[1].$m[2].$m[3];
+		}
+		$tweets[]=$tag;
+		if(count($tweets) == $cnt){return $tweets;}
+	}
+	return $tweets;
+}
+function twitterFetch($url){
+	$ch = curl_init($url);
+	curl_setopt($ch,CURLOPT_USERAGENT,"Mozilla/5.0 (Windows; U; Windows NT 5.1; pl; rv:1.9) Gecko/2008052906 Firefox/3.0"); // mask as firefox 3
+	curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);  //disable ssl certificate validation
+	curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,false);
+	curl_setopt($ch,CURLOPT_FAILONERROR,1);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+	curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);	// allow redirects
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);	// return into a variable
+	$page = curl_exec($ch);
+	if(!$page) {
+		return '';
+	}
+	return $page;
+}
 //-----------------------
 function twitterUpdateStatus($params=array()){
 	//info: update your twitter status - requires authentication
