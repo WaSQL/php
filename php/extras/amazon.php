e5f3181f944344b0ed60e01b77de64260dd75c8c
Steven Lloyd <steve.lloyd@gmail.com>
2015-03-10 12:10:21 -0600

    added amazonSendMail

--- php/extras/amazon.php
+++ php/extras/amazon.php
@@ -5,6 +5,46 @@ $progpath=dirname(__FILE__);
 /*
 	References: http://www.amazonsellercommunity.com/forums/thread.jspa?threadID=173132&tstart=1
 */
+
+
+function amazonSendMail($params=array()){
+	if(!isset($params['-accesskey'])){return "No Access Key";}
+	if(!isset($params['-secretkey'])){return "No Secret Key";}
+	if(!isset($params['to'])){return "No To";}
+	if(!isset($params['from'])){return "No From";}
+	if(!isset($params['subject'])){return "No Subject";}
+	if(!isset($params['message'])){return "No Message";}
+	$progpath=dirname(__FILE__);
+	require_once("$progpath/amazon/SimpleEmailService.php");
+	$ses = new SimpleEmailService($params['-accesskey'], $params['-secretkey']);
+	$m = new SimpleEmailServiceMessage();
+	if(!is_array($params['to'])){$params['to']=preg_split('/[\,\;]+/',$params['to']);}
+	$m->addTo($params['to']);
+	if(isset($params['cc'])){
+    	if(!is_array($params['cc'])){$params['cc']=preg_split('/[\,\;]+/',$params['cc']);}
+		$m->addCC($params['cc']);
+	}
+	if(isset($params['bcc'])){
+    	if(!is_array($params['bcc'])){$params['bcc']=preg_split('/[\,\;]+/',$params['bcc']);}
+		$m->addBCC($params['bcc']);
+	}
+	if(isset($params['attach'])){
+		if(!is_array($params['attach'])){$params['attach']=preg_split('/[\,\;]+/',$params['attach']);}
+		foreach($params['attach'] as $afile){
+        	$name=getFileName($afile);
+        	$type=getFileMimeType($afile);
+			$path=getFilePath($afile);
+			$m->addAttachmentFromFile($name, $path, $type);
+		}
+	}
+	$m->setFrom($params['from']);
+	$m->setSubject($params['subject']);
+	$m->setMessageFromString($params['message']);
+	$rtn = $ses->sendEmail($m);
+	if(isset($rtn['MessageId'])){return 1;}
+	return printValue($rtn);
+
+}
 //---------- begin function
 /**
 * @exclude  - this function is not ready and thus excluded from the manual
