b87e2265701a38f2db70cc4828ad95587236dd6d
Steven Lloyd <steve.lloyd@gmail.com>
2015-03-21 09:45:48 -0600

    POST_MAX_SIZE fixes

--- php/index.php
+++ php/index.php
@@ -1,4 +1,10 @@
 <?php
+//Set upload size
+//Set Post Max Size
+ini_set('POST_MAX_SIZE', '34M');
+ini_set('UPLOAD_MAX_FILESIZE', '30M');
+ini_set('max_execution_time', 5000);
+set_time_limit(5500);
 error_reporting(E_ALL & ~E_NOTICE);
 global $TIME_START;
 $TIME_START=microtime(true);
@@ -88,14 +94,6 @@ if(isset($_REQUEST['_pushfile'])){
 	if(isset($_REQUEST['-attach']) && $_REQUEST['-attach']==0){$params['-attach']=0;}
  	$ok=pushFile(decodeBase64($_REQUEST['_pushfile']),$params);
 }
-//Set Post Max Size
-ini_set('post_max_size', '100M');
-ini_set('upload_max_filesize', '30M');
-//Set the number of seconds a script is allowed to run
-ini_set('max_execution_time', 5000);
-set_time_limit(5500);
-//set the default time zone
-date_default_timezone_set('America/Denver');
 //Fix up REQUEST
 foreach($_REQUEST as $key=>$val){
 	if(is_array($val)){continue;}
