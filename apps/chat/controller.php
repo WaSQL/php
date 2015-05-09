2d8bb8179178ff89427be77609b8e93034a586bb
Steven Lloyd <steve.lloyd@gmail.com>
2015-05-09 00:48:04 -0600

    added config and ability to redirect messages if you are away.

--- apps/chat/controller.php
+++ apps/chat/controller.php
@@ -1,6 +1,7 @@
 <?php
 	loadExtrasCss('accordian');
 	global $USER;
+	processActions();
 	chatCheckSetup();
 	$chatTitle=isset($_REQUEST['chat_title'])?$_REQUEST['chat_title']:'chatjat';
 	if(!isUser()){
@@ -25,18 +26,34 @@
 			setView('chatlist',1);
 			return;
 		break;
+		case 'chatconfig':
+			setView('chatconfig',1);
+			return;
+		break;
+		case 'chatconfig2':
+			setView('chatconfig2',1);
+			return;
+		break;
 		case 'sendmessage':
 			$msg_to=addslashes($_REQUEST['msg_to']);
 			$msg=addslashes($_REQUEST['msg']);
 			if($msg=='*'){
 				$_REQUEST['setfocus']=$msg_to;
 			}
-			$ok=addDBRecord(array(
-				'-table'	=> 'chatlog',
-				'msg_from'	=> $USER['_id'],
-				'msg_to'	=> $msg_to,
-				'msg'		=> $msg
-			));
+			//if the msg_to is not active then send the message to backup ids instead
+			$tos=array();
+			if(chatIsUserActive($msg_to)){$tos[]=$msg_to;}
+			else{
+				$tos=chatGetBackupIds();
+			}
+			foreach($tos as $to){
+				$ok=addDBRecord(array(
+					'-table'	=> 'chatlog',
+					'msg_from'	=> $USER['_id'],
+					'msg_to'	=> $to,
+					'msg'		=> $msg
+				));
+			}
 			$chats=chatGetChatList();
 			setView('_chatlist',1);
 			if(isset($_REQUEST['formid'])){
