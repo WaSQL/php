b87cec28523529204cdb70016c04353c6dfd3b1f
Steven Lloyd <steve.lloyd@gmail.com>
2015-07-13 17:33:07 -0600

    ldabGetUsers function

--- php/extras/ldap.php
+++ php/extras/ldap.php
@@ -44,75 +44,64 @@ function ldapAuth($params=array()){
 
 	//connect
 	$params['-host']='ldap://'.$params['-host'];
-	$ldap_connection = ldap_connect($params['-host']);
-	if(!$ldap_connection){return 'LDAP Auth Error: unable to connect to host';}
+	global $ldapInfo;
+	$ldapInfo=array();
+	$ldapInfo['connection'] = ldap_connect($params['-host']);
+	if(!$ldapInfo['connection']){return 'LDAP Auth Error: unable to connect to host';}
 	// We need this for doing an LDAP search.
-	ldap_set_option($ldap_connection, LDAP_OPT_REFERRALS, 0);
-    ldap_set_option($ldap_connection, LDAP_OPT_PROTOCOL_VERSION, 3);
-    ldap_set_option($ldap_connection, LDAP_OPT_NETWORK_TIMEOUT, 5);
-    ldap_set_option($ldap_connection, LDAP_OPT_SIZELIMIT, 5);
-    ldap_set_option($ldap_connection, LDAP_OPT_TIMELIMIT, 5);
-    $enum=ldap_errno($ldap_connection);
+	ldap_set_option($ldapInfo['connection'], LDAP_OPT_REFERRALS, 0);
+    ldap_set_option($ldapInfo['connection'], LDAP_OPT_PROTOCOL_VERSION, 3);
+    ldap_set_option($ldapInfo['connection'], LDAP_OPT_NETWORK_TIMEOUT, 5);
+    ldap_set_option($ldapInfo['connection'], LDAP_OPT_SIZELIMIT, 5);
+    ldap_set_option($ldapInfo['connection'], LDAP_OPT_TIMELIMIT, 5);
+    $enum=ldap_errno($ldapInfo['connection']);
     $msg=ldap_err2str( $enum );
     //echo printValue($params);exit;
 	//bind
-    $bind=ldap_bind($ldap_connection,$params['-bind'],$params['-password']);
+    $bind=ldap_bind($ldapInfo['connection'],$params['-bind'],$params['-password']);
     if(!$bind){
-        $enum=ldap_errno($ldap_connection);
+        $enum=ldap_errno($ldapInfo['connection']);
         $msg=ldap_err2str( $enum );
-		ldap_unbind($ldap_connection); // Clean up after ourselves.
+		ldap_unbind($ldapInfo['connection']); // Clean up after ourselves.
 		return "LDAP Auth Error: auth failed. Err:{$enum}, Msg:{$msg} .. ".printValue($params);
 	}
+	foreach($params as $k=>$v){
+    	$ldapInfo[$k]=$v;
+	}
+	//set paging to 1000
+	ldap_control_paged_result($ldapInfo['connection'], 500, true, $cookie);
     //now get this users record and return it
 	$rec=array();
 	//$search_filter = "(&(objectCategory=person))";
 	//set search filter to be the current username so we get the current user record back
-	$params['search'] = "(&(objectClass=user)(sAMAccountName={$params['-username']}))";
-	$result = ldap_search($ldap_connection, $params['basedn'], $params['search']);
+	$ldapInfo['lastsearch'] = "(&(objectClass=user)(sAMAccountName={$params['-username']}))";
+	$result = ldap_search($ldapInfo['connection'], $ldapInfo['basedn'], $ldapInfo['lastsearch']);
 	//echo "result".printValue($result).printValue($params);exit;
 	if (FALSE !== $result){
-		$entries = ldap_get_entries($ldap_connection, $result);
+		$entries = ldap_get_entries($ldapInfo['connection'], $result);
 	    if ($entries['count'] == 1){
-	    	$lrec=$entries[0];
-	    	foreach($lrec as $key=>$val){
-            if(is_numeric($key)){continue;}
-            if($key=='objectguid' || $key=='count'){continue;}
-               	switch(strtolower($key)){
-                    case 'whencreated':
-                    case 'whenchanged':
-                    case 'badpasswordtime':
-                    case 'dscorepropagationdata':
-                    case 'accountexpires':
-                    case 'lastlogontimestamp':
-                    case 'lastlogon':
-                    case 'pwdlastset':
-                    	$rec[$key]=ldapValue($val);
-                    	$rec["{$key}_unix"]=ldapTimestamp($val[0]);
-                    	$rec["{$key}_date"]=date('Y-m-d h:i a',$rec["{$key}_unix"]);
-                    break;
-                    case 'memberof':
-                    	$tmp=preg_split('/\,/',ldapValue($val));
-                    	$parts=array();
-                    	foreach($tmp as $part){
-                            if(!in_array($part,$parts)){$parts[]=$part;}
-						}
-						$rec[$key]=implode(',',$parts);
-                    break;
-                    default:
-                    	$rec[$key]=ldapValue($val);
-                    break;
-				}
-			}
-			ldap_unbind($ldap_connection);
-			ksort($rec);
-			//echo "rec".printValue($rec);exit;
-			return $rec;
+	    	$rec=ldapParseEntry($entries[0]);
+	    	echo printValue($rec);ldapClose();exit;
+	    	return $rec;
 		}
-		ldap_unbind($ldap_connection); // Clean up after ourselves.
+		ldapClose();
+		//ldap_unbind($ldap_connection); // Clean up after ourselves.
     	return 'LDAP Auth Error: unable to get a unique LDAP user object'.printValue($entries);
 	}
-    ldap_unbind($ldap_connection); // Clean up after ourselves.
+	ldapClose();
     return 'LDAP Auth Error: unable to search'.printValue($result);
+}
+//---------- begin function ldapConvert2UserRecord--------------------
+/**
+* @describe converts an ldap user record to a record for the _users table
+* @param params array
+* @return array
+* @usage $rec=ldapConvert2UserRecord($rec);
+*/
+function ldapClose(){
+	global $ldapInfo;
+	ldap_unbind($ldapInfo['connection']);
+	$ldapInfo=array();
 }
 //---------- begin function ldapConvert2UserRecord--------------------
 /**
@@ -123,7 +112,7 @@ function ldapAuth($params=array()){
 */
 function ldapConvert2UserRecord($lrec=array()){
 	global $CONFIG;
-	unset($lrec['objectsid']);
+	//unset($lrec['objectsid']);
 	$rec=array('active'=>1,'utype'=>1,'ldap'=>printValue($lrec));
 	foreach($lrec as $key=>$val){
 		switch(strtolower($key)){
@@ -158,6 +147,7 @@ function ldapConvert2UserRecord($lrec=array()){
           	//case 'samaccountname':$rec['username']=$val;break;
 		}
 	}
+	unset($rec['ldap']);
 	ksort($rec);
 	return $rec;
 }
@@ -170,78 +160,84 @@ function ldapConvert2UserRecord($lrec=array()){
 * @exclude - not ready yet
 * @reference https://samjlevy.com/use-php-and-ldap-to-list-members-of-an-active-directory-group-improved/
 */
-function ldapGetRecords($group=FALSE,$inclusive=FALSE) {
-	// Active Directory server
-    $ldap_host = "ad.domain";
-    // Active Directory DN
-    $ldap_dn = "CN=Users,DC=ad,DC=domain";
-    // Domain, for purposes of constructing $user
-    $ldap_usr_dom = "@".$ldap_host;
-    // Active Directory user
-    $user = "jdoe";
-    $password = "password1234!";
-    // User attributes we want to keep
-    // List of User Object properties:
-    // http://www.dotnetactivedirectory.com/Understanding_LDAP_Active_Directory_User_Object_Properties.html
-    $keep = array(
-        "samaccountname",
-        "distinguishedname"
-    );
-    // Connect to AD
-    $ldap = ldap_connect($ldap_host) or die("Could not connect to LDAP");
-    ldap_bind($ldap,$user.$ldap_usr_dom,$password) or die("Could not bind to LDAP");
-// Begin building query
-if($group) $query = "(&"; else $query = "";
- 
-$query .= "(&(objectClass=user)(objectCategory=person))";
- 
-    // Filter by memberOf, if group is set
-    if(is_array($group)) {
-     // Looking for a members amongst multiple groups
-     if($inclusive) {
-     // Inclusive - get users that are in any of the groups
-     // Add OR operator
-     $query .= "(|";
-     } else {
-// Exclusive - only get users that are in all of the groups
-// Add AND operator
-$query .= "(&";
-     }
- 
-     // Append each group
-     foreach($group as $g) $query .= "(memberOf=CN=$g,$ldap_dn)";
- 
-     $query .= ")";
-    } elseif($group) {
-     // Just looking for membership of one group
-     $query .= "(memberOf=CN=$group,$ldap_dn)";
-    }
- 
-    // Close query
-    if($group) $query .= ")"; else $query .= "";
-
-// Uncomment to output queries onto page for debugging
-// print_r($query);
- 
-    // Search AD
-    $results = ldap_search($ldap,$ldap_dn,$query);
-    $entries = ldap_get_entries($ldap, $results);
-    // Remove first entry (it's always blank)
-    array_shift($entries);
-    $output = array(); // Declare the output array
-    $i = 0; // Counter
-    // Build output array
-    foreach($entries as $u) {
-        foreach($keep as $x) {
-         // Check for attribute
-     if(isset($u[$x][0])) $attrval = $u[$x][0]; else $attrval = NULL;
- 
-         // Append attribute to output array
-         $output[$i][$x] = $attrval;
+function ldapGetUsers($params=array()) {
+	global $ldapInfo;
+	//set the pageSize
+	ldap_get_option($ldapInfo['connection'],LDAP_OPT_SIZELIMIT,$ldapInfo['page_size']);
+	//echo $pageSize;
+	$ldapInfo['lastsearch'] = "(&(objectClass=user)(objectCategory=person))";
+	$cookie='';
+	$recs=array();
+	//loop through based on pageSize and get the records
+	do {
+        ldap_control_paged_result($ldapInfo['connection'], $ldapInfo['page_size'], true, $cookie);
+        $result = ldap_search($ldapInfo['connection'], $ldapInfo['basedn'], $ldapInfo['lastsearch']);
+        //echo printValue($cookie).printValue($ldapInfo);exit;
+        $entries = ldap_get_entries($ldapInfo['connection'], $result);
+        foreach ($entries as $e) {
+		 	$recs[]=ldapParseEntry($e);
         }
-        $i++;
-    }
-    return $output;
+    	ldap_control_paged_result_response($ldapInfo['connection'], $result, $cookie);
+
+	} while($cookie !== null && $cookie != '');
+	return $recs;
+}
+function ldapParseEntry($lrec=array()){
+	$rec=array('active'=>1,'utype'=>1);
+	foreach($lrec as $key=>$val){
+    	if(is_numeric($key)){continue;}
+        if($key=='objectguid' || $key=='objectsid' || $key=='msexchsafesendershash' || $key=='count'){continue;}
+        switch(strtolower($key)){
+            case 'whencreated':
+            case 'whenchanged':
+            case 'badpasswordtime':
+            case 'dscorepropagationdata':
+            case 'accountexpires':
+            case 'lastlogontimestamp':
+            case 'lastlogon':
+            case 'pwdlastset':
+                $rec[$key]=ldapValue($val);
+                $rec["{$key}_unix"]=ldapTimestamp($val[0]);
+                $rec["{$key}_date"]=date('Y-m-d h:i a',$rec["{$key}_unix"]);
+            break;
+            case 'memberof':
+                $tmp=preg_split('/\,/',ldapValue($val));
+                $parts=array();
+                foreach($tmp as $part){
+                    if(!in_array($part,$parts)){$parts[]=$part;}
+				}
+				$rec[$key]=implode(',',$parts);
+            break;
+            case 'samaccountname':$rec['username']=$val;break;
+          	case 'dn':$rec['password']=substr(encodeBase64($val),10,30);break;
+          	case 'l':$rec['city']=$val;break;
+          	case 'c':$rec['country']=$val;break;
+          	case 'sn':$rec['lastname']=$val;break;
+          	case 'givenname':$rec['firstname']=$val;break;
+          	case 'displayname':$rec['name']=$val;break;
+          	case 'department':$rec['department']=$val;break;
+          	case 'title':$rec['title']=$val;break;
+          	case 'mail':$rec['email']=$val;break;
+          	case 'company':$rec['company']=$val;break;
+          	case 'homephone':$rec['phone_home']=$val;break;
+          	case 'mobile':$rec['phone_mobile']=$val;break;
+          	case 'postalcode':$rec['zip']=$val;break;
+          	case 'st':$rec['state']=$val;break;
+          	case 'streetaddress':$rec['address1']=$val;break;
+          	case 'telephonenumber':$rec['phone']=$val;break;
+          	case 'url':$rec['url']=$val;break;
+          	case 'primarygroupid':$rec['primarygroupid']=$val;break;
+          	case 'manager':
+          		if(preg_match('/CN\=(.+?)\,/',$val,$m)){$rec['manager']=$m[1];}
+          		elseif(preg_match('/CN\=(.+?)$/',$val,$m)){$rec['manager']=$m[1];}
+				else{$rec['manager']=$val;}
+			break;
+            default:
+                $rec[$key]=ldapValue($val);
+            break;
+		}
+	}
+	return $rec;
 }
 function ldapTimestamp($ad) {
 	if(stringContains($ad,'.')){
