44546b8beb7d6dcd734b2649db75de8765c518fd
Steven Lloyd <steve.lloyd@gmail.com>
2015-08-01 20:02:14 -0600

    documentation

--- php/extras/ftp.php
+++ php/extras/ftp.php
@@ -117,15 +117,48 @@ function ftpGetFile($params=array()){
 	}
 }
 //---------- begin function ftpGetFiles
+/**
+* @describe gets all files in remote_dir and places it in local_dir
+* @param params array
+*	-server string - ftp server to connect to
+*	-user string - username
+*	-pass string - password
+*	-local_dir - specify directory to place local file in
+*	-remote_dir string - name of remote directory to list. Defaults to .
+*	[-move_dir] string - path to move the remote file to after fetching it
+*	[-delete] boolean - delete the remote file to after fetching it
+* @return filepath string - location of the local file
+* @usage 
+*	<?php
+*	$file=ftpGetFiles(array(
+*		'-server'	=> 'ftp.myserver.com',
+*		'-user'		=> 'myusername',
+*		'-pass'		=> 'mypassword',
+*		'-remote_dir'=>'/var/home/temp',
+*		'-local_file'=>'myfile.txt'
+*	));
+*	?>
+*/
 function ftpGetFiles($params=array()){
 	//info: gets FTP files on remote server in remote dir and places in in local_dir - returns and array
-	if(!isset($params['remote_dir'])){$params['remote_dir']='.';}
-	$conn_id = ftp_connect($params['server']);
-	$login = ftp_login($conn_id, $params['user'], $params['pass']);
+	//backward compatibility
+	if(isset($params['server'])){$params['-server']=$params['server'];}
+	if(isset($params['user'])){$params['-user']=$params['user'];}
+	if(isset($params['pass'])){$params['-pass']=$params['pass'];}
+	if(isset($params['remote_dir'])){$params['-remote_dir']=$params['remote_dir'];}
+	if(isset($params['remote_file'])){$params['-remote_file']=$params['remote_file'];}
+	if(isset($params['local_dir'])){$params['-local_dir']=$params['local_dir'];}
+	if(isset($params['local_file'])){$params['-local_file']=$params['local_file'];}
+	if(isset($params['delete'])){$params['-delete']=$params['delete'];}
+	if(isset($params['move_dir'])){$params['-move_dir']=$params['move_dir'];}
+	//defaults
+	if(!isset($params['-remote_dir'])){$params['-remote_dir']='.';}
+	$conn_id = ftp_connect($params['-server']);
+	$login = ftp_login($conn_id, $params['-user'], $params['-pass']);
 	// turn passive mode on
 	ftp_pasv($conn_id, true);
 	//change directory
-	if(!ftp_chdir($conn_id,$params['remote_dir'])){return "ftpGetFile Error: cannot chdir to {$params['remote_dir']}";}
+	if(!ftp_chdir($conn_id,$params['-remote_dir'])){return "ftpGetFile Error: cannot chdir to {$params['-remote_dir']}";}
 	// get list of files
 	$files = ftp_nlist($conn_id, '.');
 	$localfiles=array();
@@ -136,12 +169,12 @@ function ftpGetFiles($params=array()){
 		$mode=FTP_BINARY;
 		if(isTextFile($file)){$mode=FTP_ASCII;}
 		// try to download $server_file and save to $local_file
-		$local_file="{$params['local_dir']}/{$file}";
+		$local_file="{$params['-local_dir']}/{$file}";
 		if (ftp_get($conn_id, $local_file, $remote_file, $mode)) {
 		    $localfiles[]=$local_file;
-		    if($params['move_dir']){
+		    if($params['-move_dir']){
 				//move the file
-				$move_file="{$params['move_dir']}/{$remote_file}";
+				$move_file="{$params['-move_dir']}/{$remote_file}";
 				ftp_rename($conn_id, $remote_file, $move_file);
             }
 		}
@@ -154,23 +187,54 @@ function ftpGetFiles($params=array()){
 	return $localfiles;
 }
 //---------- begin function ftpPutFile
+/**
+* @describe uploads a single  file to a remote_dir
+* @param params array
+*	-server string - ftp server to connect to
+*	-user string - username
+*	-pass string - password
+*	-local_file - name of local file to save as
+*	-remote_file - name of remote file to get
+*	[-remote_dir] string - name of remote directory to list. Defaults to .
+* @return filepath string - location of the remote file
+* @usage 
+*	<?php
+*	$file=ftpPutFile(array(
+*		'-server'	=> 'ftp.myserver.com',
+*		'-user'		=> 'myusername',
+*		'-pass'		=> 'mypassword',
+*		'-remote_dir'=>'/var/home/temp',
+*		'-local_file'=>'myfile.txt'
+*	));
+*	?>
+*/
 function ftpPutFile($params=array()){
+	//backward compatibility
+	if(isset($params['server'])){$params['-server']=$params['server'];}
+	if(isset($params['user'])){$params['-user']=$params['user'];}
+	if(isset($params['pass'])){$params['-pass']=$params['pass'];}
+	if(isset($params['remote_dir'])){$params['-remote_dir']=$params['remote_dir'];}
+	if(isset($params['remote_file'])){$params['-remote_file']=$params['remote_file'];}
+	if(isset($params['local_dir'])){$params['-local_dir']=$params['local_dir'];}
+	if(isset($params['local_file'])){$params['-local_file']=$params['local_file'];}
+	if(isset($params['delete'])){$params['-delete']=$params['delete'];}
+	if(isset($params['move_dir'])){$params['-move_dir']=$params['move_dir'];}
 	//info: gets FTP files on remote server in remote dir and places in in local_dir - returns and array
-	$conn_id = ftp_connect($params['server']);
-	$login = ftp_login($conn_id, $params['user'], $params['pass']);
+	$conn_id = ftp_connect($params['-server']);
+	$login = ftp_login($conn_id, $params['-user'], $params['-pass']);
 	// turn passive mode on
 	ftp_pasv($conn_id, true);
 	//change directory
-	if(isset($params['remote_dir']) && !ftp_chdir($conn_id,$params['remote_dir'])){return "ftpPutFile Error: cannot chdir to {$params['remote_dir']}";}
+	if(isset($params['-remote_dir']) && !ftp_chdir($conn_id,$params['-remote_dir'])){return "ftpPutFile Error: cannot chdir to {$params['-remote_dir']}";}
 	//Set mode
 	$mode=FTP_BINARY;
-	if(isTextFile($params['local_file'])){$mode=FTP_ASCII;}
+	if(isTextFile($params['-local_file'])){$mode=FTP_ASCII;}
 	//upload the file
-	if(ftp_put($conn_id,$params['remote_file'],$params['local_file'],$mode)){
+	if(ftp_put($conn_id,$params['-remote_file'],$params['-local_file'],$mode)){
 		ftp_close($conn_id);
-    	return $params['remote_file'];
+    	return $params['-remote_file'];
 	}
-	else{return "ftpPutFile Error: put failed {$params['remote_file']}";}
+	else{return "ftpPutFile Error: put failed {$params['-remote_file']}";}
 	ftp_close($conn_id);
 	return null;
 }
