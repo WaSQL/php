b05adfbe22d416ee060fd88997789d7f95584f99
Steven Lloyd <steve.lloyd@gmail.com>
2015-10-01 22:32:11 -0600

    added paths in PATH to INC so that local:lib works

--- piegen.pl
+++ piegen.pl
@@ -36,3 +36,35 @@ print "Content-type: image/gif\n\n";
 binmode STDOUT;
 print $gd_image->gif;
 exit(0);
+#############
+BEGIN {
+	#add path to INC
+	my @parts=split(/\:/,$ENV{PATH});
+	foreach $part (@parts){
+    	unshift(@INC,$part);
+	}
+	our ($temp_dir,$progpath,$progexe,$progname,$isexe)=('','','','',0);
+	$temp_dir = ( $ENV{TEMP} || $ENV{TMP} || $ENV{WINDIR} || '/tmp' ) . "/p2xtmp-$$";
+	$0 = $^X unless ($^X =~ m%(^|[/\\])(perl)|(perl.exe)$%i);
+	($progpath) = $0 =~ m%^(.*)[/\\]%;
+	$progpath ||= ".";
+	unshift(@INC,$progpath);
+	$progname=lc($0);
+	if($progname=~/[\/\\]/){
+		my @stmp=split(/[\/\\]/,$progname);
+		$progname=pop(@stmp);
+		}
+	$progname=~s/\Q$progpath\E//s;
+	$progname=~s/^[\\\/]+//s;
+	if($progname=~/(.+?)\.(exe|pl|so)/is){$progname=$1;}
+	if ($^X =~ /(perl)|(perl\.exe)$/i) {
+		$progexe=$progname . '.pl';
+		$isexe=0;
+		}
+	else{
+		#Compiled
+		$isexe=1;
+		@INC=($progpath,"./lib","./","PERL2EXE_STORAGE",$temp_dir);
+		$progexe=$progname . '.exe';
+		}
+	}
