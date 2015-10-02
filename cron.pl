989f78a9f67a6b3fe0c1d7a0924a6490cfb45540
Steven Lloyd <steve.lloyd@gmail.com>
2015-10-01 22:32:10 -0600

    added paths in PATH to INC so that local:lib works

--- cron.pl
+++ cron.pl
@@ -386,6 +386,11 @@ sub abort {
 	}
 #############
 BEGIN {
+	#add path to INC
+	my @parts=split(/\:/,$ENV{PATH});
+	foreach $part (@parts){
+    	unshift(@INC,$part);
+	}
 	our ($temp_dir,$progpath,$progexe,$progname,$isexe)=('','','','',0);
 	$temp_dir = ( $ENV{TEMP} || $ENV{TMP} || $ENV{WINDIR} || '/tmp' ) . "/p2xtmp-$$";
 	$0 = $^X unless ($^X =~ m%(^|[/\\])(perl)|(perl.exe)$%i);
@@ -407,8 +412,7 @@ BEGIN {
 	else{
 		#Compiled
 		$isexe=1;
-		my @tmp=@INC;
-		@INC=($progpath,"./lib","./","PERL2EXE_STORAGE",$temp_dir,'d:/perl/site/lib','d:/perl/lib','c:/perl/site/lib','c:/perl/lib');
+		@INC=($progpath,"./lib","./","PERL2EXE_STORAGE",$temp_dir);
 		$progexe=$progname . '.exe';
 		}
 	}
