cfa79ec5511d824e007e167671afdb7e936b0a33
Steven Lloyd <steve.lloyd@gmail.com>
2015-10-01 22:32:11 -0600

    added paths in PATH to INC so that local:lib works

--- env.pl
+++ env.pl
@@ -9,6 +9,12 @@ foreach my $key (sort(keys(%ENV))){
 exit;
 #############
 BEGIN {
+	#add path to INC
+	my @parts=split(/\:/,$ENV{PATH});
+	foreach $part (@parts){
+    	unshift(@INC,$part);
+	}
+	our ($temp_dir,$progpath,$progexe,$progname,$isexe)=('','','','',0);
 	$temp_dir = ( $ENV{TEMP} || $ENV{TMP} || $ENV{WINDIR} || '/tmp' ) . "/p2xtmp-$$";
 	$0 = $^X unless ($^X =~ m%(^|[/\\])(perl)|(perl.exe)$%i);
 	($progpath) = $0 =~ m%^(.*)[/\\]%;
@@ -24,6 +30,13 @@ BEGIN {
 	if($progname=~/(.+?)\.(exe|pl|so)/is){$progname=$1;}
 	if ($^X =~ /(perl)|(perl\.exe)$/i) {
 		$progexe=$progname . '.pl';
+		$isexe=0;
+		}
+	else{
+		#Compiled
+		$isexe=1;
+		@INC=($progpath,"./lib","./","PERL2EXE_STORAGE",$temp_dir);
+		$progexe=$progname . '.exe';
 		}
 	}
 
