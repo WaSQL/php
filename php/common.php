de34b5b0ba5ea1c27595a1c420a62ea247c59c3b
Steven Lloyd <steve.lloyd@gmail.com>
2015-10-26 13:20:51 -0600

    added jsonCheck function

--- php/common.php
+++ php/common.php
@@ -6998,6 +6998,57 @@ function isGDEnabled(){
     	}
 	return false;
 	}
+//---------- begin function jsonCheck ----------
+/**
+* @describe returns true if string is a valid JSON. returns error if not.
+* @return mixed
+*	returns true if GD is enabled. GD must be enabled to create images, etc.
+* @usage if(isGDEnabled($str)){...}
+*/
+function jsonCheck($string){
+    // decode the JSON data
+    $result = json_decode($string);
+    // switch and check possible JSON errors
+    switch (json_last_error()) {
+        case JSON_ERROR_NONE:
+            $error = ''; // JSON is valid // No error has occurred
+            break;
+        case JSON_ERROR_DEPTH:
+            $error = 'The maximum stack depth has been exceeded.';
+            break;
+        case JSON_ERROR_STATE_MISMATCH:
+            $error = 'Invalid or malformed JSON.';
+            break;
+        case JSON_ERROR_CTRL_CHAR:
+            $error = 'Control character error, possibly incorrectly encoded.';
+            break;
+        case JSON_ERROR_SYNTAX:
+            $error = 'Syntax error, malformed JSON.';
+            break;
+        // PHP >= 5.3.3
+        case JSON_ERROR_UTF8:
+            $error = 'Malformed UTF-8 characters, possibly incorrectly encoded.';
+            break;
+        // PHP >= 5.5.0
+        case JSON_ERROR_RECURSION:
+            $error = 'One or more recursive references in the value to be encoded.';
+            break;
+        // PHP >= 5.5.0
+        case JSON_ERROR_INF_OR_NAN:
+            $error = 'One or more NAN or INF values in the value to be encoded.';
+            break;
+        case JSON_ERROR_UNSUPPORTED_TYPE:
+            $error = 'A value of a type that cannot be encoded was given.';
+            break;
+        default:
+            $error = 'Unknown JSON error occured.';
+            break;
+    }
+    if ($error !== '') {
+		return $error;
+    }
+	return true;
+}
 //---------- begin function isLiveUrl ----------
 /**
 * @describe returns true if $url is valid (not broken) - assumes internet connection is present
