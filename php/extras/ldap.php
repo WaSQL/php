5013c488f89e2d0b9b3aa529375933141dd64d8c
Steven Lloyd <steve.lloyd@gmail.com>
2015-10-28 08:48:59 -0600

    userpassword set in ldapAddUser

--- php/extras/ldap.php
+++ php/extras/ldap.php
@@ -29,6 +29,10 @@ function ldapAddUser($params){
 	if(!isset($params['objectclass'])){
 		$params['objectclass']= array("top","person","organizationalPerson","user");
 	}
+	//user password
+	if(!isset($params['userpassword']) && isset($params['password'])){
+		$params["userpassword"] = '{md5}' . base64_encode(pack('H*', md5($params['password'])));
+	}
 	//prevent the user from being disabled
 	$params['UserAccountControl']="512";
 	//call ldap_add to add the entry
