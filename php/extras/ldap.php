86702665eb82a4e29b2833db5a5d0888fabd8206
Steven Lloyd <steve.lloyd@gmail.com>
2015-12-03 10:31:59 -0700

    init array

--- php/extras/ldap.php
+++ php/extras/ldap.php
@@ -372,6 +372,7 @@ function ldapParseEntry($lrec=array()){
                 $parts=array();
                 foreach($tmp as $part){
 					list($k,$v)=preg_split('/\=/',$part,2);
+					if(!is_array($parts[$k])){$parts[$k]=array();}
                     if(!in_array($v,$parts[$k])){$parts[$k][]=$v;}
 				}
 				foreach($parts as $k=>$v){
