1975bf8a40b589bd8510b86883d1d9b772029c20
Steven Lloyd <steve.lloyd@gmail.com>
2016-02-15 16:56:31 -0700

    stripe changes

--- php/common.php
+++ php/common.php
@@ -8019,14 +8019,6 @@ function loadExtrasJs($extras){
         			$extra='https://ajax.googleapis.com/ajax/libs/angularjs/1.3.9/angular.js';
 				}
         	break;
-        	case 'stripe':
-        		if($CONFIG['minify_js']){
-        			$extra='https://js.stripe.com/v1/';
-				}
-				else{
-        			$extra='https://js.stripe.com/v1/';
-				}
-        	break;
         	case 'signature':
 			case 'file':
 				$extra='html5';
