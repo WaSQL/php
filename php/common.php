e24a33dd045d80aaf852dd6836ce3de6a6729b15
Steven Lloyd <steve.lloyd@gmail.com>
2016-05-10 12:58:10 -0600

    documentation

--- php/common.php
+++ php/common.php
@@ -208,6 +208,27 @@ function buildIECompatible($version=0){
 * @describe
 *	returns a JSON data structure to use with ChartJs charts
 * @param recs array - array of record sets. each record set needs three fields: xval, yval, and setval
+* @param [params] - optional parameters
+*		{setval}_attribute will set for the following attributes
+*			fill 	Boolean 	If true, fill the area under the line
+*			lineTension 	Number 	Bezier curve tension of the line. Set to 0 to draw straightlines. Note This was renamed from 'tension' but the old name still works.
+*			backgroundColor 	Color 	The fill color under the line. You can specify the color as a string in hexadecimal, RGB, or HSL notations
+*			borderWidth 	Number 	The width of the line in pixels
+*			borderColor 	Color 	The color of the line.
+*			borderCapStyle 	String 	Cap style of the line.
+*			borderDash 	Array<Number> 	Length and spacing of dashes.
+*			borderDashOffset 	Number 	Offset for line dashes.
+*			borderJoinStyle 	String 	Line joint style.
+*			pointBorderColor 	Color or Array<Color> 	The border color for points.
+*			pointBackgroundColor 	Color or Array<Color> 	The fill color for points
+*			pointBorderWidth 	Number or Array<Number> 	The width of the point border in pixels
+*			pointRadius 	Number or Array<Number> 	The radius of the point shape. If set to 0, nothing is rendered.
+*			pointHoverRadius 	Number or Array<Number> 	The radius of the point when hovered
+*			hitRadius 	Number or Array<Number> 	The pixel size of the non-displayed point that reacts to mouse events
+*			pointHoverBackgroundColor 	Color or Array<Color> 	Point background color when hovered
+*			pointHoverBorderColor 	Color or Array<Color> 	Point border color when hovered
+*			pointHoverBorderWidth 	Number or Array<Number> 	Border width of point when hovered
+*			pointStyle 	String or Array<String> 	The style of point. Options include 'circle', 'triangle', 'rect', 'rectRot', 'cross', 'crossRot', 'star', 'line', and 'dash'
 * @return json string
 * @usage $buildChartJsData($recs);
 */
@@ -239,6 +260,12 @@ function buildChartJsData($recs,$params=array()){
 	$xkeys=array_flip($data['labels']);
 	foreach($vrecs as $setval=>$recs){
     	$dataset=array('label'=>$setval);
+    	//add any params for this dataset - fill, borderDash, hidden
+    	foreach($params as $k=>$v){
+        	if(preg_match('/^'.$setval.'_(.+)$/i',$k,$m)){
+            	$dataset[$m[1]]=$v;
+			}
+		}
     	$cdata=array();
     	//fill in in case
     	foreach($xkeys as $i){
