74ee99b9962929acab39dea0c836fbe9730fbad9
Steven Lloyd <steve.lloyd@gmail.com>
2016-12-31 12:22:38 -0700

    fixed include path

--- php/extras/websockets/websockets_client.php
+++ php/extras/websockets/websockets_client.php
@@ -1,6 +1,6 @@
 <?php
 $progpath=dirname(__FILE__);
-require_once("{$progpath}/websockets/websockets_functions.php");
+require_once("{$progpath}/websockets_functions.php");
 //get command line arguments, removing the first one since it is the filename
 array_shift($argv);
 //if the first arg is a file assume they want to tail the file
