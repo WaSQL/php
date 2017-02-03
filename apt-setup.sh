48ecb5420ff339d50c3fd4df0f597561e95656d9
Steven Lloyd <steve.lloyd@gmail.com>
2017-02-03 08:45:38 -0700

    scripts to install dependant modules

---
+++ apt-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#make sure curl and cli are installed
+apt-get install php-curl php-cli
+#make sure the perl modules are installed
+apt-get install libimage-exif-perl libspreadsheet-writeexcel-perl libtext-csv-xs-perlhistor libgd-graph-perl
+
