0a37520ec10c58ca8346b2baf2f837386ca6d8d3
Steven Lloyd <steve.lloyd@gmail.com>
2017-02-03 08:46:37 -0700

    scripts to install dependant modules

---
+++ yum-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#make sure curl and cli are installed
+yum install php-curl php-cli
+#make sure the perl modules are installed
+yum install libimage-exif-perl libspreadsheet-writeexcel-perl libtext-csv-xs-perlhistor libgd-graph-perl
+
