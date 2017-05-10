f3c8bd7d298b6605b73fefb3360ae032df3661c8
Steven Lloyd <steve.lloyd@gmail.com>
2017-05-09 22:15:32 -0600

    moved export to the admin mvc architecture

---
+++ php/admin/export_controller.php
@@ -0,0 +1,25 @@
+<?php
+
+	switch(strtolower($_REQUEST['func'])){
+		case 'export':
+			setView('export',1);
+			//export_schema, export_meta, export_data
+			//export_pages, export_templates
+			$filename=addslashes($_REQUEST['name']);
+			$schema=isset($_REQUEST['export_schema']) && is_array($_REQUEST['export_schema']) && count($_REQUEST['export_schema'])?$_REQUEST['export_schema']:array();
+			$meta=isset($_REQUEST['export_meta']) && is_array($_REQUEST['export_meta']) && count($_REQUEST['export_meta'])?$_REQUEST['export_meta']:array();
+			$data=isset($_REQUEST['export_data']) && is_array($_REQUEST['export_data']) && count($_REQUEST['export_data'])?$_REQUEST['export_data']:array();
+			$pages=isset($_REQUEST['export_pages']) && is_array($_REQUEST['export_pages']) && count($_REQUEST['export_pages'])?$_REQUEST['export_pages']:array();
+			$templates=isset($_REQUEST['export_templates']) && is_array($_REQUEST['export_templates']) && count($_REQUEST['export_templates'])?$_REQUEST['export_templates']:array();
+			$ok=pageBuildExport($filename,$schema,$meta,$data,$pages,$templates);
+			return;
+		break;
+		default:
+			$tables=pageGetTables();
+			$epages=pageGetPages();
+			$etemplates=pageGetTemplates();
+			setView('default',1);
+		break;
+	}
+	setView('default',1);
+?>
