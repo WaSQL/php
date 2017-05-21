cabeb177b3a3c18d611c52961b3c014197f14a11
Steven Lloyd <steve.lloyd@gmail.com>
2017-05-20 22:13:29 -0600

    redid wasql documentation to use MVC

--- php/admin/manual_controller.php
+++ php/admin/manual_controller.php
@@ -1,12 +1,58 @@
 <?php
 global $progpath;
+$docfile="{$progpath}/temp/docfile.json";
 switch(strtolower($_REQUEST['func'])){
 	case 'search':
+		$docs=json_decode(getFileContents($docfile),true);
+		if(!is_array($docs)){
+			echo "failed to decode docs";exit;
+		}
+		$search=$_REQUEST['search'];
+		$sdocs=array();
+		//echo "HERE".printValue($docs);exit;
+		foreach($docs as $file=>$functions){
+			if(!isset($functions[0])){
+				echo "HERE".printValue($functions);exit;
+			}
+			foreach($functions as $function){
+				$found=0;
+				foreach($function as $k=>$v){
+					if(is_array($v)){
+						foreach($v as $vv){
+							if(stringContains(removeHtml($vv),$search)){
+								$found++;
+								continue;
+							}
+						}
+					}
+					else{
+						if(stringContains(removeHtml($v),$search)){
+							$found++;
+							continue;
+						}
+					}
+				}
+				if($found){
+					$sdocs[]=$function;
+				}
+			}
+		}
 		setView('search',1);
 		return;
 	break;
 	default:
+		global $docs;
+		$docs=array();
 		$files=listFilesEx($progpath,array('ext'=>'php'));
+		foreach($files as $file){
+			$ok=manualParseFile($file['afile']);
+		}
+		$files=listFilesEx("{$progpath}/extras",array('ext'=>'php'));
+		foreach($files as $file){
+			$ok=manualParseFile($file['afile']);
+		}
+		$content=json_encode($docs);
+		setFileContents($docfile,$content);
 		setView('default',1);
 	break;
 }
