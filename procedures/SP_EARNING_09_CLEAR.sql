157ad110e451748291be63ed8e9d6238b9e17689
Larry <lcardon@doterra.com>
2017-07-27 13:11:55 -0600

    Binary Truncate Dump Issue

--- procedures/SP_EARNING_09_CLEAR.sql
+++ procedures/SP_EARNING_09_CLEAR.sql
@@ -1,4 +1,4 @@
-DROP PROCEDURE SP_EARNING_09_CLEAR;
+drop procedure Commissions.sp_Earning_09_Clear;
 create procedure Commissions.sp_Earning_09_Clear(
 					 pn_Period_id		int
 					,pn_Period_Batch_id	int)
