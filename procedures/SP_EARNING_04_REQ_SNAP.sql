d746c3fbafaf78f4b95996d421db283a53552508
Larry <lcardon@doterra.com>
2017-07-27 13:11:54 -0600

    Binary Truncate Dump Issue

--- procedures/SP_EARNING_04_REQ_SNAP.sql
+++ procedures/SP_EARNING_04_REQ_SNAP.sql
@@ -1,4 +1,4 @@
-DROP PROCEDURE SP_EARNING_04_REQ_SNAP;
+drop procedure Commissions.sp_Earning_04_Req_Snap;
 create procedure Commissions.sp_Earning_04_Req_Snap(
 					 pn_Period_id		int)
    LANGUAGE SQLSCRIPT
