c3e788ba3f50635af0b6526c21a63675bd929689
Steven Lloyd <slloyd@doterra.com>
2017-07-27 10:47:39 -0600

    resnap from functions in database

---
+++ procedures/SP_EARNING_03_CLEAR.sql
@@ -0,0 +1,23 @@
+DROP PROCEDURE SP_EARNING_03_CLEAR;
+create procedure Commissions.sp_Earning_03_Clear(
+					 pn_Period_id		int
+					,pn_Period_Batch_id	int)
+   LANGUAGE SQLSCRIPT
+   DEFAULT SCHEMA Commissions
+AS
+
+begin
+
+	update customer_history
+	set Earning_3 = 0
+	where period_id = :pn_Period_id
+	and batch_id = :pn_Period_Batch_id;
+	
+	delete
+	from Earning_03
+	where period_id = :pn_Period_id
+	and batch_id = :pn_Period_Batch_id;
+	  
+	commit;
+
+end;
