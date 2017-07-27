177997e2809687df9336295219b1c67998558f03
Steven Lloyd <steve.lloyd@gmail.com>
2017-07-27 15:06:35 -0600

    adding wcmd for a wasql command runner

---
+++ wcmd.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+#usage: wcmd.pl cmd path
+$progname="wcmd";
+$|=1;
+require "subs_common.pl";
+print cmdResults(@ARGV[0],@ARGV[1]);
+exit;
+
+#############
+BEGIN {
+	#add path to INC
+	my @parts=split(/\:/,$ENV{PATH});
+	foreach $part (@parts){
+    	unshift(@INC,$part);
+	}
+	our ($temp_dir,$progpath,$progexe,$progname,$isexe)=('','','','',0);
+	$temp_dir = ( $ENV{TEMP} || $ENV{TMP} || $ENV{WINDIR} || '/tmp' ) . "/p2xtmp-$$";
+	$0 = $^X unless ($^X =~ m%(^|[/\\])(perl)|(perl.exe)$%i);
+	($progpath) = $0 =~ m%^(.*)[/\\]%;
+	$progpath ||= ".";
+	unshift(@INC,$progpath);
+	$progname=lc($0);
+	if($progname=~/[\/\\]/){
+		my @stmp=split(/[\/\\]/,$progname);
+		$progname=pop(@stmp);
+		}
+	$progname=~s/\Q$progpath\E//s;
+	$progname=~s/^[\\\/]+//s;
+	if($progname=~/(.+?)\.(exe|pl|so)/is){$progname=$1;}
+	if ($^X =~ /(perl)|(perl\.exe)$/i) {
+		$progexe=$progname . '.pl';
+		$isexe=0;
+		}
+	else{
+		#Compiled
+		$isexe=1;
+		@INC=($progpath,"./lib","./","PERL2EXE_STORAGE",$temp_dir);
+		$progexe=$progname . '.exe';
+		}
+	}
+###########
