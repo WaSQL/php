83166f510f4f7d22233e885b69f8072aee72e940
Steven Lloyd <steve.lloyd@gmail.com>
2017-09-16 11:14:41 -0600

    name fix for getWebsiteMeta

--- php/common.php
+++ php/common.php
@@ -55,7 +55,10 @@ function getWebsiteMeta($url){
 		if(isset($meta['meta']['name']) && strlen($meta['meta']['name'])){$meta['summary']['name']=$meta['meta']['name'];}
 		elseif(isset($meta['meta']['og:site_name']) && strlen($meta['meta']['og:site_name'])){$meta['summary']['name']=$meta['meta']['og:site_name'];}
 		elseif(isset($meta['meta']['twitter:site']) && strlen($meta['meta']['twitter:site'])){$meta['summary']['name']=$meta['meta']['twitter:site'];}
-		elseif(isset($meta['summary']['title']) && strlen($meta['summary']['title'])){$meta['summary']['name']=$meta['summary']['title'];}
+		elseif(isset($meta['summary']['title']) && strlen($meta['summary']['title'])){
+			$p=preg_split('/[\-\:]+/',$meta['summary']['title'],2);
+			$meta['summary']['name']=trim($p[0]);
+		}
 		else{$meta['summary']['name']=getUniqueHost($meta['base_url']);}
 	}
 	if(!isset($meta['summary']['description']) || !strlen($meta['summary']['description'])){
