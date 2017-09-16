e8799d0c7ca45cd4348eb5cb8054ca96b4d39ade
Steven Lloyd <steve.lloyd@gmail.com>
2017-09-16 10:10:26 -0600

    image path fix for getWebsiteMeta

--- php/common.php
+++ php/common.php
@@ -38,6 +38,7 @@ function getWebsiteMeta($url){
 			break;
 		}
 		if(strlen($k) && $k != 'stylesheet'){
+			$k=strtolower($k);
 			$meta[$g][$k]=$v;
 		}
 	}
@@ -64,10 +65,15 @@ function getWebsiteMeta($url){
 		elseif(isset($meta['meta']['og:image']) && strlen($meta['meta']['og:image'])){$meta['summary']['image']=$meta['meta']['og:image'];}
 		elseif(isset($meta['meta']['twitter:image:src']) && strlen($meta['meta']['twitter:image:src'])){$meta['summary']['image']=$meta['meta']['twitter:image:src'];}
 		elseif(isset($meta['link']['apple-touch-icon']) && strlen($meta['link']['apple-touch-icon'])){$meta['summary']['image']=$meta['link']['apple-touch-icon'];}
+		elseif(isset($meta['meta']['apple-touch-icon-precomposed']) && strlen($meta['meta']['apple-touch-icon-precomposed'])){$meta['summary']['image']=$meta['meta']['apple-touch-icon-precomposed'];}
+		elseif(isset($meta['meta']['msapplication-tileimage']) && strlen($meta['meta']['msapplication-tileimage'])){$meta['summary']['image']=$meta['meta']['msapplication-tileimage'];}
 		elseif(isset($meta['link']['icon']) && strlen($meta['link']['icon'])){$meta['summary']['image']=$meta['link']['icon'];}
 	}
 	if(isset($meta['summary']['image']) && strlen($meta['summary']['image']) && !preg_match('/^(http|https|\/\/)/',$meta['summary']['image'])){
-		$meta['summary']['image']=$url.$meta['summary']['image'];
+		//remove any anchors
+		$p=preg_split('/\#/',$url,2);
+		$base_url=$p[0];
+		$meta['summary']['image']=$base_url.$meta['summary']['image'];
 	}
 	$meta['summary']['image']=preg_replace('/^http\:\/\//i',"[!a!]",$meta['summary']['image']);
 	$meta['summary']['image']=preg_replace('/^https\:\/\//i',"[!b!]",$meta['summary']['image']);
