273f2fa89e1fe572eb6c373d2d315f60be7eb86e
Steven Lloyd <steve.lloyd@gmail.com>
2017-09-21 19:44:13 -0600

    fix so cron can be parallel processing

---
+++ php/cron.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#get the path of the script
+if [ -L $0 ] ; then
+    ME=$(readlink $0)
+else
+    ME=$0
+fi
+SDIR=$(dirname $ME)
+#start the cron servers
+cd $SDIR
+
+php cron.php &
+php cron.php &
+sleep 5
+php cron.php &
+php cron.php &
+sleep 5
+php cron.php &
+php cron.php &
+sleep 5
+php cron.php &
+php cron.php &
+sleep 5
+php cron.php &
+php cron.php &
+sleep 5
+php cron.php &
+php cron.php &
+sleep 5
+php cron.php &
+php cron.php &
+sleep 5
+php cron.php &
+php cron.php &
+sleep 5
+php cron.php &
+php cron.php &
+sleep 5
+php cron.php &
+php cron.php &
+sleep 5
+php cron.php &
+php cron.php &
+
