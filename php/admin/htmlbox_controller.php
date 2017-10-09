5ea26975c3ac54caaadfa607bbccb25b029bd754
Steven Lloyd <steve.lloyd@gmail.com>
2017-10-09 06:52:23 -0600

    changed layout of html sandox to make is cleaner and have more room

--- php/admin/htmlbox_controller.php
+++ php/admin/htmlbox_controller.php
@@ -1,6 +1,13 @@
 <?php
 	$cpath=dirname(__FILE__);
 	switch(strtolower($_REQUEST['func'])){
+		case 'save':
+			$_SESSION['htmlbox_css']=$_REQUEST['htmlbox_css'];
+			$_SESSION['htmlbox_html']=$_REQUEST['htmlbox_html'];
+			$htmlref=getCSVFileContents("{$cpath}/admin/htmlref.csv");
+        	$cssref=getCSVFileContents("{$cpath}/admin/cssref.csv");
+			setView('default',1);
+		break;
         default:
         	$htmlref=getCSVFileContents("{$cpath}/admin/htmlref.csv");
         	$cssref=getCSVFileContents("{$cpath}/admin/cssref.csv");
