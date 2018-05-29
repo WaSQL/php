7c2fa3b40561f0fc8d468799b0ee806f77338792
Steven Lloyd <steve.lloyd@gmail.com>
2018-05-29 11:44:42 -0600

    fix for php extension

--- postedit/postedit.php
+++ postedit/postedit.php
@@ -175,9 +175,12 @@ function writeFiles(){
 	    	if(!is_dir($path)){
 				mkdir($path,0777,true);
 			}
+			$content=base64_decode(trim($content));
+			//check content to see if it starts with php
+			if(preg_match('/^\<\?php/i',$content)){$ext='php';}
 	    	$afile="{$path}/{$name}.{$info['table']}.{$field}.{$info['_id']}.{$ext}";
 	    	//echo "{$afile}".PHP_EOL;
-	    	$content=base64_decode(trim($content));
+	    	
 	    	file_put_contents($afile,$content);
 	    	$mtimes[$afile]=1;
 		}
