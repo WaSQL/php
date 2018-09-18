41b4da93a6a1bd828632b844766239fb581a7915
Steven Lloyd <steve.lloyd@gmail.com>
2018-09-18 15:05:50 -0600

    tooltip fix

--- wfiles/js/event.js
+++ wfiles/js/event.js
@@ -436,7 +436,7 @@ function tooltipDiv(obj,rtimer){
     	var att=str_replace('att:','',txt);
     	txt=obj.getAttribute(att) || '';
 	}
-	if(txt.length === 0 || txt==='false' || !txt){txt=' ';}
+	//if(txt.length === 0 || txt==='false' || !txt){txt=' ';}
 	var cObj=getObject('w_tooltip');
 	if(undefined != cObj){removeId(cObj);}
 	var tipdiv = document.createElement("div");
