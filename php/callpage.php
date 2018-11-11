3666b93d25afa4b7ad57fb283710884968be42d7
Steven Lloyd <steve.lloyd@gmail.com>
2018-11-10 21:45:39 -0700

    added callpage.php for accessing pages from cmd line - for use in crons

--- php/callpage.php
+++ php/callpage.php
@@ -3,6 +3,10 @@
 	Instructions:
 		run callpage.php from a command-line to make it easier to run a cron page
 		Note: callpage.php cannot be run from a URL, it is a command line app only.
+	Example
+		php callpage.php -host=localhost -page=testme/a/b/c name=bob age=8
+	Crontab Example running every minute
+		* * * * * php /var/www/wasql/php/callpage.php -host=localhost -page=testme/a/b/c name=bob age=8 >/var/www/wasql/php/callpage.log 2>&1
 */
 //set time limit to a large number so the callpage does not time out
 ini_set('max_execution_time', 72000);
