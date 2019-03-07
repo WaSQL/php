acb8234d69559c7b8b41ae0b51fb2bc2abfa760c
Steven Lloyd <steve.lloyd@gmail.com>
2019-03-07 16:29:43 -0700

    bind variable fix for illegial chars

--- php/extras/oracle.php
+++ php/extras/oracle.php
@@ -118,24 +118,6 @@ function oracleAddDBRecord($params){
 				}
 				$descriptor[$k]->writeTemporary($values[$k]);
     		break;
-    		case 'number':
-    		case 'integer':
-    			if(!oci_bind_by_name($stid, "{$bind}", $values[$k], strlen($values[$k]), OCI_B_INT )){
-			    	debugValue(array(
-			    		'function'=>"oracleAddDBRecord",
-			    		'connection'=>$dbh_oracle,
-			    		'stid'=>$stid,
-			    		'action'=>'oci_bind_by_name',
-			    		'oci_error'=>oci_error($stid),
-			    		'query'=>$query,
-			    		'field'=>$k,
-			    		'_dbtype'=>$fields[$k]['_dbtype'],
-			    		'bind'=>$bind,
-			    		'value'=>$values[$k]
-			    	));
-			    	return false;
-				}
-    		break;
     		case 'blob':
     			if(!oci_bind_by_name($stid, "{$bind}", $values[$k], strlen($values[$k]), OCI_B_BLOB )){
 			    	debugValue(array(
@@ -154,7 +136,7 @@ function oracleAddDBRecord($params){
 				}
     		break;
     		default:
-    			if(!oci_bind_by_name($stid, "{$bind}", $values[$k], strlen($values[$k]))){
+    			if(!oci_bind_by_name($stid, $bind, $values[$k])){
 			    	debugValue(array(
 			    		'function'=>"oracleAddDBRecord",
 			    		'connection'=>$dbh_oracle,
