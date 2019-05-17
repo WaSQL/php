598fa9ffed479d63f62646bde2f9eac66dd6e356
Steven Lloyd <steve.lloyd@gmail.com>
2019-05-17 08:01:57 -0600

    relative path on w.bat and recompress wacss

--- w.bat
+++ w.bat
@@ -1,2 +1,2 @@
-php d:\minifier\minifyme.php d:\wasql\wfiles\css\extras\wacss.css
-php d:\minifier\minifyme.php d:\wasql\wfiles\js\extras\wacss.js
+php ..\minifier\minifyme.php ..\wasql\wfiles\css\extras\wacss.css
+php ..\minifier\minifyme.php ..\wasql\wfiles\js\extras\wacss.js
