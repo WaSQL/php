eb31503fafcd86470b93c6669c38563d769432ee
Steven Lloyd <steve.lloyd@gmail.com>
2019-10-30 05:06:00 -0600

    js errors fix for bulma. added small support to btn buttons

--- wfiles/js/extras/bulma.js
+++ wfiles/js/extras/bulma.js
@@ -1,10 +1,14 @@
 // The following code is based off a toggle menu by @Bradcomp
 // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
 (function() {
-    var burger = document.querySelector('.burger');
-    var menu = document.querySelector('#'+burger.dataset.target);
-    burger.addEventListener('click', function() {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
-    });
+    let burger = document.querySelector('.burger');
+    if(undefined != burger){
+	    burger.addEventListener('click', function() {
+	        this.classList.toggle('is-active');
+	        let menu=document.querySelector('#'+this.getAttribute('data-target'));
+	        if(undefined != menu){
+	        	menu.classList.toggle('is-active');
+	        }
+	    });
+	}
 })();
