f96aaf726188ebe47b5dea1e89b35864c3f1010c
Steven Lloyd <steve.lloyd@gmail.com>
2020-01-02 10:54:52 -0700

    add sorting to crons

--- php/admin/cron_functions.php
+++ php/admin/cron_functions.php
@@ -71,6 +71,7 @@ function cronList(){
 	$opts=array(
 		'-table'=>'_cron',
 		'-fields'=>'_id,groupname,name,active,paused,running,run_date,run_length,run_cmd,records_to_keep,run_error',
+		'-searchfields'=>'_id,groupname,name,active,paused,running,records_to_keep',
 		'-listfields'=>'_id,groupname,name,err,active,paused,running,last_run,run_length,run_cmd,records_to_keep',
 		'-tableclass'=>'table striped bordered',
 		'-action'=>$url,
