e0f88707194b71300391a5c3b4f77a4047052697
Steven Lloyd <steve.lloyd@gmail.com>
2020-01-27 13:22:14 -0700

    added dbISQL

--- php/database.php
+++ php/database.php
@@ -61,10 +61,13 @@ function dbISQL($dsn,$user,$pass,$query){
 	//isql prd3t1hana tableau T8bl3au123 -d, -c </var/www/wasql_stage/php/temp/q.sql >/var/www/wasql_stage/php/temp/q.csv
 	$path=getWasqlPath('php/temp');
 	$b64=base64_encode($query);
+	$b64=str_replace('=','',$b64);
 	$qfile="{$path}/{$b64}.sql";
 	$ok=setFileContents($qfile,$query);
-	$cmd="isql {$dsn} {$user} {$pass} -d, -c '<{$qfile}'";
+	$cmd="isql {$dsn} {$user} {$pass} -d, -c <'{$qfile}'";
+	//echo $cmd;exit;
 	$out=cmdResults($cmd);
+	//echo printValue($out);exit;
 	unlink($qfile);
 	$lines=preg_split('/[\r\n]/',$out['stdout']);
 	$found=0;
@@ -79,7 +82,11 @@ function dbISQL($dsn,$user,$pass,$query){
 	if(stringBeginsWith($lines[$last], 'SQL>')){
 		array_pop($lines);
 	}
+	//lowercase the fields (first line)
+	$lines[0]=strtolower($lines[0]);
+	echo count($lines).printValue($lines);
 	$recs=csv2Arrays($lines);
+	echo printValue($recs);exit;
 	return $recs;
 }
 //---------- db functions that allow you to pass in what database first
