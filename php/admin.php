32d176bf2121160d63af581903a991a7479ffa6f
Steven Lloyd <steve.lloyd@gmail.com>
2020-02-24 17:09:57 -0700

    fix auth error

--- php/admin.php
+++ php/admin.php
@@ -394,7 +394,7 @@ ENDOFQUERY;
 			//request to get sync changes
 			if(!isUser()){
 				//echo printValue($_REQUEST);exit;
-				echo base64_encode(json_encode(array('error'=>"Login Failed. Please try again")));
+				echo base64_encode(json_encode(array('error'=>"Login Failed. Please try again. [{$_REQUEST['_auth']}]")));
 				foreach($_SESSION as $k=>$v){
 					if(preg_match('/^(sync\_|git\_)/i',$k)){
 						unset($_SESSION[$k]);
