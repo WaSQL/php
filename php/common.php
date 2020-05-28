acdd716bf28984b5a050f6e391e6e7dd92bffc8a
Steven Lloyd <steve.lloyd@gmail.com>
2020-05-28 11:20:35 -0600

    added getallheaders replacement if function does not exist

--- php/common.php
+++ php/common.php
@@ -1,5 +1,19 @@
 <?php
-/**/
+//---------- getallheaders replacement for PHP-FPM ---------------------------------------
+/**
+* @exclude  - this function is excluded from the manual
+*/
+if (!function_exists('getallheaders')) {
+    function getallheaders() {
+    $headers = [];
+    foreach ($_SERVER as $name => $value) {
+        if (substr($name, 0, 5) == 'HTTP_') {
+            $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+        }
+    }
+    return $headers;
+    }
+}
 
 //---------- begin function commonBuildTerminal
 /**
