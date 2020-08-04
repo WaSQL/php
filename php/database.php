fb3dcbd6581f0f8ae90bd34de9486a9f0493ddd1
Steven Lloyd <steve.lloyd@gmail.com>
2020-08-04 14:56:28 -0600

    postgres fix for is blank searches

--- php/database.php
+++ php/database.php
@@ -3575,7 +3575,15 @@ function databaseParseFilters($params=array()){
 			case 'nb':
 				//is not blank
 				if(!isNum($val)){
-					$wheres[]="{$field} is not null and LENGTH({$field}) > 0";
+					switch(strtolower($params['-database'])){
+						case 'postgres':
+						case 'postgresql':
+							$wheres[]="{$field} is not null and LENGTH({$field}::text) > 0";
+						break;
+						default:
+							$wheres[]="{$field} is not null and LENGTH({$field}) > 0";
+						break;
+					}
 				}
 			break;
 		}
