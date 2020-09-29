488abdf4d718148861a345b3761538dbcb58bcf2
Steven Lloyd <steve.lloyd@gmail.com>
2020-09-29 17:13:59 -0600

    added logs viewer

--- php/admin/logs_controller.php
+++ php/admin/logs_controller.php
@@ -1,7 +1,7 @@
 <?php
 	global $CONFIG;
 	$refresh=isset($CONFIG['logs_refresh'])?(integer)$CONFIG['logs_refresh']:60;
-	if($refresh < 15){$refresh=15;}
 	$logs=logsGetLogs();
 	setView('default');
+	if($refresh < 15){setView('refresh');}
 ?>
