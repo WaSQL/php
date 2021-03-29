94815a949d9d08212554a9a0dcdd343d6c3872cd
Steven Lloyd <steve.lloyd@gmail.com>
2021-03-28 21:35:13 -0600

    change to cleanmin so it should work wherever

--- sh/cleanmin.sh
+++ sh/cleanmin.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-cd /var/www
-for d in * ; do
-	if [ -d "/var/www/$d/w_min" ] 
+if [ -d "c:/AppServ/www" ]; then 
+	basepath="c:/AppServ/www"
+else
+	full_path=$(realpath "$0")
+	dir_path=$(dirname "$full_path")
+	wasqlpath=$(dirname "$dir_path" )
+	basepath=$(dirname "$wasqlpath" )
+fi
+
+echo "Basepath: $basepath"
+
+if [ -d "$basepath/w_min" ] 
+then
+    echo "cleaning $basepath/w_min"
+	rm -f "$basepath/w_min/*.*" 
+fi
+basefiles="$basepath/*"
+for sdir in $basefiles ; do
+	if [ -d "$basepath/$sdir/w_min" ] 
 	then
-	    echo "cleaning /var/www/$d/w_min"
-    	rm -f /var/www/$d/w_min/*.* 
+	    echo "cleaning $basepath/$sdir/w_min"
+    	rm -f "$basepath/$sdir/w_min/*.*" 
 	fi
 done
