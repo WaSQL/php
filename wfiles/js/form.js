5e8b4c92a692c84436bb192b8c54e1a4549f44f5
Steven Lloyd <steve.lloyd@gmail.com>
2021-11-23 20:07:23 -0700

    added debug to formDictate

--- wfiles/js/form.js
+++ wfiles/js/form.js
@@ -633,13 +633,6 @@ function formDictate(inp,ico,frm,continuous,debug) {
 	    	recognition.frm=frm;
 	    }
       	recognition.inp=inp;
-      	recognition.start() => VM187: 1 err: SpeechRecognitionErrorEvent {
-		   isTrusted: true,
-		   error: "not-allowed",
-		   message: "",
-		   type: "error",
-		   target: SpeechRecognition
-		};
       	//recognition.start();
       	recognition.debug=debug;
       	recognition.onresult = function(e) {
