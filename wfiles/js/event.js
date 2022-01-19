37bac09f5c1c7c9f952fb2fef8613df543253091
Steven Lloyd <steve.lloyd@gmail.com>
2022-01-19 12:35:08 -0700

    added firstdayofweek and weeknumbers

--- wfiles/js/event.js
+++ wfiles/js/event.js
@@ -2572,6 +2572,12 @@ function initFlatpickr(){
 				case 'disablemobile':k='disableMobile';break;
 				case 'enabletime':k='enableTime';break;
 				case 'enableseconds':k='enableSeconds';break;
+				case 'firstdayofweek':
+				case 'firstday':
+					config.locale.firstDayOfWeek=v;
+					continue;
+					//k='firstDayOfWeek';
+				break;
 				case 'hourincrement':k='hourIncrement';break;
 				case 'maxdate':
 					k='maxDate';
@@ -2613,7 +2619,10 @@ function initFlatpickr(){
 				case 'prevarrow':k='prevArrow';break;
 				case 'shorthandcurrentmonth':k='shorthandCurrentMonth';break;
 				case 'showmonths':k='showMonths';break;
-				case 'weeknumbers':k='weekNumbers';break;
+				case 'weeknumbers':
+				case 'showweeknumber':
+					k='weekNumbers';
+				break;
 				case 'monthselectortype':k='monthSelectorType';break;
 				default:continue;break;
 			}
