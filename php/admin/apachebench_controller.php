5c08d48844eafe48fea7c8d1c356d00ac6e46368
Steven Lloyd <steve.lloyd@gmail.com>
2022-03-10 18:59:30 -0700

    update ab to allow payloads

--- php/admin/apachebench_controller.php
+++ php/admin/apachebench_controller.php
@@ -1,11 +1,40 @@
 <?php
-	if(isset($_REQUEST['requests'])){
+/*
+	https://httpd.apache.org/docs/2.4/programs/ab.html
+*/
+switch(strtolower($_REQUEST['func'])){
+	case 'run':
 		$n=(integer)$_REQUEST['requests'];
 		$c=(integer)$_REQUEST['concurrency'];
 		$url=$_REQUEST['url'];
-		$cmd="ab -w -q -n {$n} -c {$c} {$url}";
+		$args=array('-w','-q',"-n {$n}","-c {$c}");
+		$tpath=getWasqlTempPath();
+		$pfile="{$tpath}/apachebench.txt";
+		if(strlen($_REQUEST['payload'])){
+			setFileContents($pfile,$_REQUEST['payload']);
+			$args[]="-p \"{$pfile}\"";
+			//set content-type
+			$j=json_decode($_REQUEST['payload'],true);
+			if(is_array($j)){
+				$args[]="-T \"application/json\"";
+			}
+			else{
+				$args[]="-T \"application/x-www-form-urlencoded\"";	
+			}
+
+		}
+		if(strlen($_REQUEST['proxy'])){
+			$args[]="-X \"{$_REQUEST['proxy']}\"";
+		}
+		if(strlen($_REQUEST['proxy_auth'])){
+			$args[]="-P \"{$_REQUEST['proxy_auth']}\"";
+		}
+		$args[]=$url;
+		$argstr=implode(' ',$args);
+		$cmd="ab {$argstr}";
 		//echo $cmd;exit;
 		$out=cmdResults($cmd);
+		unlink($pfile);
 		if(isset($out['stderr'])){
 			$result=$out['stderr'];
 			if(preg_match('/(.+?)Usage:/ism',$result,$m)){
@@ -23,9 +52,9 @@
 			}
 		}
 		setView('result',1);
-	}
-	else{
-		apachebenchCheckSchema();
+	break;
+	default:
 		setView('default');
-	}
+	break;
+}
 ?>
