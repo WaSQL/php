07954b283967a9d416f2be3bf5324db4fb28736e
Steven Lloyd <steve.lloyd@gmail.com>
2022-06-05 22:10:03 -0600

    cleanup

--- php/admin/config_controller.php
+++ php/admin/config_controller.php
@@ -1,43 +1,4 @@
 <?php
-/*
-	User authentication: 
-		Method: ldap
-			host
-			domain
-			checkmemberof
-			admins
-		Method: OKTA
-			okta_client_id="0oa51shizpAKNJdjS5d7"
-        	okta_client_secret="tS3s7hN66E8b-vzfkMKenqds5AUju4fcjqbgvL_9"
-        	okta_metadata_url="https://dev-67363231.okta.com/oauth2/default/.well-known/openid-configuration"
-        	okta_redirect_uri="https://localhost/"
-        	okta_restrict_auto_login_duration="30"
-        	ldap_username="serviceterra"
-        	ldap_password="0nG@dm1ns!"
-        	ldap_host="ldap.corp.doterra.net"
-        	ldap_domain="corp.doterra.net"
-        	ldap_secure="0"
-        	ldap_checkmemberof="0"
-        	ldap_basedn="DC=corp,DC=doterra,DC=net"
-        	admins
-        login_title
-        userlog
-
-	Sendmail authentication
-		Method: aws
-		Method: smtp
-			user
-			pass
-			port
-		from
-		encrypt
-		phpmailer
-	file conversions
-		convert
-		convert_command
-		reencode
-		reencode_command	
-*/
 	global $CONFIG;
 	global $DATABASE;
 	global $USER;
