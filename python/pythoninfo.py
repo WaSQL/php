f6488e033444ff46a45e9c07b0d76ad5fd8e02ce
Steven Lloyd <steve.lloyd@gmail.com>
2022-09-06 07:08:42 -0600

    added pythoninfo to admin menu so it parses out like phypinfo does

--- python/pythoninfo.py
+++ python/pythoninfo.py
@@ -21,25 +21,29 @@ prows=''
 for p in pkg_resources.working_set:
 	if p.project_name=='fdb':
 		continue
+	
 	prow='<div class="align-center w_bold" style="margin-top:10px;font-size:clamp(11px,2vw,24px);color:#1d415e"><a name="module_{}">{}</a></div><table class="table striped condensed" style="border:1px solid #000;margin-top:5px;">'.format(p.project_name,p.project_name)
-	try:     
-		for line in subprocess.check_output(['python3','-m','pip','show',p.project_name]).decode('utf-8').split('\n'):
-			if "no module named pip" in line:
-				print("pip is not installed. aborting")
-				sys.exit(123)
-			parts=line.split(':',1)
-			partscount=len(parts)
-			if partscount != 2:
-				continue;
-			key=parts[0]
-			val=parts[1]
-			trow='<tr><td style="background:#1d415e;color:#FFF;white-space:nowrap;padding-right:0.4rem;">{}</td><td style="width:100%;">{}</td></tr>'.format(key,val)
-			prow=prow+trow
+	info=''
+	try:
+		info = pkg_resources.get_distribution(p.project_name).get_metadata('PKG-INFO')
 	except Exception as err:
-		prow=''
+		info = pkg_resources.get_distribution(p.project_name).get_metadata('METADATA')
+
+	for line in info.splitlines():
+		parts=line.split(':',1)
+		partscount=len(parts)
+		if partscount != 2:
+			continue;
+		if parts[0]=='Classifier':
+			break;
+		key=parts[0]
+		val=parts[1]
+		trow='<tr><td style="background:#1d415e;color:#FFF;white-space:nowrap;padding-right:0.4rem;">{}</td><td style="width:100%;">{}</td></tr>'.format(key,val)
+		prow=prow+trow
 
 	prow=prow+"</table>"
 	prows=prows+prow
+
 
 output="""
 <section>
