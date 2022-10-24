f68c62bc8f010cefda4f544c1826a7bed8b2f986
Steven Lloyd <steve.lloyd@gmail.com>
2022-10-24 07:38:02 -0600

    add button to show full script in languages

--- php/admin/test_controller.php
+++ php/admin/test_controller.php
@@ -11,9 +11,30 @@
 			case 'chartjs':
 				
 			break;
+			case 'script':
+				$lang=commonGetLangInfo($_REQUEST['lang']);
+				switch(strtolower($lang['name'])){
+					case 'python':
+						$lang['code']="print ('Hello, world!')".PHP_EOL."print(wasql.user('username'))".PHP_EOL;
+					break;
+					case 'nodejs':
+						$lang['code']="console.log(process.version);".PHP_EOL;
+					break;
+					case 'perl':
+						$lang['code']="print ('Hello, world!<br>');".PHP_EOL."print wasqlUser('username');".PHP_EOL;
+					break;
+					case 'lua':
+						$lang['code']="print('Hello, world!');".PHP_EOL."var2=wasqlUser('username');".PHP_EOL."print(var2);".PHP_EOL;
+					break;
+					case 'ruby':
+						$lang['code']="puts 'Hello, world!'".PHP_EOL;
+					break;
+				}
+				setView('script',1);
+				return;
+			break;
 			case 'language_includes':
 				$lang=commonGetLangInfo($_REQUEST['lang']);
-				//echo printValue($_REQUEST);exit;
 				setView('language_includes',1);
 				return;
 			break;
