bd7bf0f719e1280422271f5745ddc528521da533
Steven Lloyd <steve.lloyd@gmail.com>
2023-04-18 07:45:25 -0600

    fix unset variables check

--- php/common.php
+++ php/common.php
@@ -14409,8 +14409,8 @@ function commonIncludeFunctionCode($content,$name=''){
 			if(!strlen($evalcode)){continue;}
 			$evalcode='<?'.'php'.PHP_EOL.$evalcode;	
 			$content_md5=md5($evalcode);
-
-			$tmpfile="{$CONFIG['name']}_php_{$content_md5}.php";
+			$tname=isset($CONFIG['name'])?$CONFIG['name']:'NO_CONFIG_NAME';
+			$tmpfile="{$tname}_php_{$content_md5}.php";
 			$afile="{$tmppath}/{$tmpfile}";
 			$afile=str_replace("\\","/",$afile);
 			//echo $afile.PHP_EOL;
@@ -15023,15 +15023,15 @@ function isFactor($num=0,$divisor=1){
 */
 function isMobileDevice(){
 	// check if the user agent value claims to be windows but not windows mobile
-	if(stristr($_SERVER['HTTP_USER_AGENT'],'windows') && !stristr($_SERVER['HTTP_USER_AGENT'],'windows ce')){
+	if(isset($_SERVER['HTTP_USER_AGENT']) && stristr($_SERVER['HTTP_USER_AGENT'],'windows') && !stristr($_SERVER['HTTP_USER_AGENT'],'windows ce')){
 		return false;
 		}
 	// check if the user agent gives away any tell tale signs it's a mobile browser
-	if(preg_match('/(up\.browser|up\.link|windows ce|iemobile|mini|mmp|blackberry|symbian|midp|wap|phone|ipad|iphone|pocket|mobile|pda|psp|silk\-accelerated)/i',$_SERVER['HTTP_USER_AGENT'])){
+	if(isset($_SERVER['HTTP_USER_AGENT']) && preg_match('/(up\.browser|up\.link|windows ce|iemobile|mini|mmp|blackberry|symbian|midp|wap|phone|ipad|iphone|pocket|mobile|pda|psp|silk\-accelerated)/i',$_SERVER['HTTP_USER_AGENT'])){
 		return true;
 		}
 	// check the http accept header to see if wap.wml or wap.xhtml support is claimed
-	if(stristr($_SERVER['HTTP_ACCEPT'],'text/vnd.wap.wml')||stristr($_SERVER['HTTP_ACCEPT'],'application/vnd.wap.xhtml+xml')){
+	if(isset($_SERVER['HTTP_ACCEPT']) && stristr($_SERVER['HTTP_ACCEPT'],'text/vnd.wap.wml')||stristr($_SERVER['HTTP_ACCEPT'],'application/vnd.wap.xhtml+xml')){
 		return true;
 		}
 	// check if there are any tell tales signs it's a mobile device from the _server headers
