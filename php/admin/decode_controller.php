3c16406e30163a78e7ea6402845f8f63a83b5009
Steven Lloyd <steve.lloyd@gmail.com>
2023-09-30 09:01:43 -0600

    added QRCode and Barcode encoder to the backend

--- php/admin/decode_controller.php
+++ php/admin/decode_controller.php
@@ -2,12 +2,12 @@
 	switch(strtolower($_REQUEST['func'])){
 		case 'json_forms':
 			$type='json';
-			setView('forms',1);
+			setView('decode_forms',1);
 			return;
 		break;
 		case 'base64_forms':
 			$type='base64';
-			setView('forms',1);
+			setView('decode_forms',1);
 			return;
 		break;
 		case 'json':
@@ -20,6 +20,44 @@
 		case 'base64':
 			$decoded=base64_decode($_REQUEST['base64']);
 			setView('decoded',1);
+			return;
+		break;
+		case 'qrcode_form':
+			$type='qrcode';
+			setView('encode_form',1);
+			return;
+		break;
+		case 'qrcode':
+			loadExtras('qrcode');
+			$lines=preg_split('/[\r\n]+/',trim($_REQUEST['content']));
+			$codes=array();
+			foreach($lines as $url){
+				$qrcode=qrcodeCreate($url,'','H',5);
+				$b64=encodeBase64($qrcode);
+				$codes[]=<<<ENDOFCODE
+<div style="margin-bottom:20px;display:flex;flex-direction:column;align-items:center;">
+	<div><img src="data:image/png;base64,{$b64}"></div>
+	<div class="w_bold">{$url}</div>
+</div>
+ENDOFCODE;
+			}
+			$results='<div style="display:flex;flex-direction:column;align-items:flex-start;">'.implode(PHP_EOL,$codes).'</div>';
+			setView('results',1);
+			return;
+		break;
+		case 'barcode_form':
+			$type='barcode';
+			setView('encode_form',1);
+			return;
+		break;
+		case 'barcode':
+			$lines=preg_split('/[\r\n]+/',trim($_REQUEST['content']));
+			$codes=array();
+			foreach($lines as $code){
+				$codes[]="<div style=\"margin-bottom:30px;\"><img src=\"/php/barcode.php?{$code}\"></div>";
+			}
+			$results='<div style="display:flex;flex-direction:column;align-items:flex-start;">'.implode(PHP_EOL,$codes).'</div>';
+			setView('results',1);
 			return;
 		break;
 		default:
