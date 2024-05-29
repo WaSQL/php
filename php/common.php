6a9f3030c0bce1b8e28a8fc0188db19f91ce0a0d
Steven Lloyd <steve.lloyd@gmail.com>
2024-05-28 21:13:01 -0600

    use HTML5 required on checkbox and radio

--- php/common.php
+++ php/common.php
@@ -3254,7 +3254,7 @@ function buildFormRadioCheckbox($name, $opts=array(), $params=array()){
 			$rtn .= ' data-displayif="'.$displayif.'"';
 		}
 		$rtn.='>'.PHP_EOL;
-		$rtn.='		<input style="width:0px;height:0px;display:block;align-self:center;" type="'.$params['-type'].'" id="'.$opt_id.'" name="'.$input_name.'" value="'.$tval.'"';
+		$rtn.='		<input style="width:1px;height:1px;display:block;align-self:center;" type="'.$params['-type'].'" id="'.$opt_id.'" name="'.$input_name.'" value="'.$tval.'"';
 		//required
 		if(isset($params['required']) && $params['required']){
 			$rtn .= ' required="required"';
