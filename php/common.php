62bb4cb55da3944d38a9bae2654ee7fa18703321
Steven Lloyd <steve.lloyd@gmail.com>
2025-10-07 19:44:48 -0600

    fix for buildFormFileImage to fix style

--- php/common.php
+++ php/common.php
@@ -4403,7 +4403,7 @@ ENDOFTAG;
 function buildFormFileImage($name,$params=array()){
 	if(!isset($params['-formname'])){$params['-formname']='addedit';}
 	if(!isset($params['-icon'])){$params['-icon']='icon-upload w_big w_danger';}
-	if(!isset($params['-style'])){$params['-style']='height:40px;';}
+	if(!isset($params['-style'])){$params['-style']='height:40px;width:100%;';}
 	if(!isset($params['text'])){
 		if(isset($params['multiple'])){$params['text']='Browse Images';}
 		else{$params['text']='Browse';}
