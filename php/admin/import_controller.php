82ee6f09cef35b678ccfddde8cc45b8a14398ac8
Steven Lloyd <slloyd88@users.noreply.github.com>
2025-12-22 18:24:57 -0700

    added File Type Validation and access control

--- php/admin/import_controller.php
+++ php/admin/import_controller.php
@@ -1,13 +1,27 @@
 <?php
+//Access control check
+if(!isAdmin()){
+	echo '<div class="w_bold w_danger">Access Denied: Admin privileges required</div>';
+	exit;
+}
+
 global $importrecs;
 global $importrecs_total;
 global $results;
 global $fieldinfo;
 $importrecs=$results=array();
+
+//Validate file type parameter
+$validFileTypes=array('xml','csv','apps');
+$fileType=isset($_REQUEST['filetype'])?strtolower($_REQUEST['filetype']):'';
+if(!in_array($fileType,$validFileTypes)){
+	$fileType='xml'; //default to xml
+}
+
 switch(strtolower($_REQUEST['func'])){
 	case 'process':
 		$ok=processFileUploads();
-		switch(strtolower($_REQUEST['filetype'])){
+		switch($fileType){
 			case 'xml':
 				$results=importProcessXML($_REQUEST);
 			break;
