a1eb3b05e0984bcc610f7a738e6bb20797f9fdd9
Steven Lloyd <slloyd88@users.noreply.github.com>
2025-12-24 14:46:40 -0700

    Fix for  SQL injection and XSS vulnerabilities

--- modules/sysmon/model.php
+++ modules/sysmon/model.php
@@ -5,14 +5,17 @@ function sysmonGetProcessList(){
 	if(!empty($_REQUEST['_filters'])){
 		$filters=array();
         //field-oper-value
-        if(is_array($params['_filters'])){$sets=$_REQUEST['_filters'];}
+        if(is_array($_REQUEST['_filters'])){$sets=$_REQUEST['_filters'];}
     	else{$sets=preg_split('/[\r\n\,]+/',$_REQUEST['_filters']);}
     	foreach($sets as $set){
         	list($field,$oper,$val)=preg_split('/\-/',trim($set),3);
+        	//validate field and operator to prevent injection
+        	$valid_opers=array('ct','nct','eq','neq','gt','lt','egt','elt');
+        	if(!in_array($oper,$valid_opers)){continue;}
         	$filters[]=array(
-        		'field'=>$field,
-        		'oper'=>$oper,
-        		'val'=>$val
+        		'field'=>trim($field),
+        		'oper'=>trim($oper),
+        		'val'=>trim($val)
         	);
         }
         foreach($recs as $i=>$rec){
@@ -53,14 +56,17 @@ function sysmonGetProcessList(){
 	}
 	//order by
 	if(isset($_REQUEST['filter_order']) && strlen($_REQUEST['filter_order'])){
-		$parts=preg_split('/\ /',$_REQUEST['filter_order']);
-		if(count($parts)==2 && $parts[1]=='desc'){
-			$recs=sortArrayByKey($recs,$parts[0],SORT_DESC);
+		$parts=preg_split('/\ /',trim($_REQUEST['filter_order']));
+		//validate sort order to prevent injection
+		$valid_fields=array('command','pid','pcpu','pmem','session_name','status','user');
+		if(count($parts) >= 1 && in_array($parts[0],$valid_fields)){
+			if(count($parts)==2 && strtolower($parts[1])=='desc'){
+				$recs=sortArrayByKey($recs,$parts[0],SORT_DESC);
+			}
+			else{
+				$recs=sortArrayByKey($recs,$parts[0],SORT_ASC);
+			}
 		}
-		else{
-			$recs=sortArrayByKey($recs,$parts[0],SORT_ASC);
-		}
-		
 	}
 	return $recs;
 }
@@ -90,51 +96,62 @@ function sysmonProcessList($recs){
 	);
 	return databaseListRecords($opts);
 }
+/**
+ * sysmonProcessListExtra - callback function for additional processing of process list records
+ * This function is called by databaseListRecords via -results_eval parameter
+ * Add custom record processing logic here if needed
+ */
 function sysmonProcessListExtra($recs){
 	global $PAGE;
-	foreach($recs as $i=>$rec){
-		
-	}
+	//add custom processing here if needed
 	return $recs;
 }
 
 function sysmonMountsList(){
 	//note: df is piped to a file then read to get around splitting issue on some platforms
 	$path=getWasqlPath('php/temp');
-	$outfile="{$path}/dfb1.txt";
-	$cmd=<<<ENDOFCMD
-df -B1 | awk '{print \$1","\$2","\$3","\$4","\$5","\$6","\$7","\$8}' >{$outfile}
-ENDOFCMD;
-	$cmd=trim($cmd);
+	if(!is_dir($path)){
+		return '<div class="w_red w_bold">Error: temp directory not found</div>';
+	}
+	//sanitize path to prevent command injection
+	$outfile=realpath($path).DIRECTORY_SEPARATOR.'dfb1.txt';
+	$cmd=sprintf("df -B1 | awk '{print \$1\",\"\$2\",\"\$3\",\"\$4\",\"\$5\",\"\$6\",\"\$7\",\"\$8}' >%s",escapeshellarg($outfile));
 	$out=`$cmd`;
-	if(file_exists($outfile)){
-		$lines=file($outfile);
+	if(!file_exists($outfile)){
+		return '<div class="w_red w_bold">Error: unable to retrieve disk information</div>';
+	}
+	$lines=file($outfile);
+	if($lines===false){
 		unlink($outfile);
-		foreach($lines as $i=>$line){
-			if($i==0){continue;}
-			$line=trim($line);
-			$line=preg_replace('/\,+$/','',$line);
-			$parts=preg_split('/\,/',$line);
-			if(count($parts) > 6){
-				$c=count($parts) - 5;
-				$xparts=array();
-				for($x=0;$x<$c;$x++){
-					$xparts[]=$parts[$x];
-				}
-				$nparts=array(implode(' ',$xparts));
-				for($x=$c;$x<count($parts);$x++){
-					$nparts[]=$parts[$x];
-				}
-				$lines[$i]=implode(',',$nparts);
+		return '<div class="w_red w_bold">Error: unable to read disk information</div>';
+	}
+	unlink($outfile);
+	foreach($lines as $i=>$line){
+		if($i==0){continue;}
+		$line=trim($line);
+		$line=preg_replace('/\,+$/','',$line);
+		$parts=preg_split('/\,/',$line);
+		if(count($parts) > 6){
+			$c=count($parts) - 5;
+			$xparts=array();
+			for($x=0;$x<$c;$x++){
+				$xparts[]=$parts[$x];
 			}
+			$nparts=array(implode(' ',$xparts));
+			for($x=$c;$x<count($parts);$x++){
+				$nparts[]=$parts[$x];
+			}
+			$lines[$i]=implode(',',$nparts);
 		}
 	}
 	$out=implode(PHP_EOL,$lines);
 	$recs=CSV2Arrays($out);
+	if(empty($recs)){
+		return '<div class="w_red w_bold">Error: unable to parse disk information</div>';
+	}
 	$recs=array_change_key_case($recs,CASE_LOWER);
 	foreach($recs as $i=>$rec){
 		$recs[$i]=array_change_key_case($recs[$i],CASE_LOWER);
-		//echo printValue($rec);exit;
 		$recs[$i]['1b-blocks']=verboseSize($recs[$i]['1b-blocks']);
 		$recs[$i]['used']=verboseSize($recs[$i]['used']);
 		$recs[$i]['available']=verboseSize($recs[$i]['available']);
@@ -146,10 +163,11 @@ ENDOFCMD;
 		elseif($pcnt > 60){
 			$bgcolor='#ffc107';
 		}
-		$pcntstr='<div class="w_right">'.$recs[$i]['use%'].'</div>';
-		//add bar
+		$pcntstr='<div class="w_right">'.encodeHtml($recs[$i]['use%']).'</div>';
+		//add bar - sanitize pcnt to prevent XSS
+		$pcnt_safe=min(100,max(0,(integer)$pcnt));
 		$recs[$i]['use%']='<div style="border:1px solid #ccd2d9;height:15px;width:150px;display:inline-block;">';
-		$recs[$i]['use%'].='<div style="disply:inline-block;height:15px;width:'.$pcnt.'%;background-color:'.$bgcolor.';"></div>';
+		$recs[$i]['use%'].='<div style="display:inline-block;height:15px;width:'.$pcnt_safe.'%;background-color:'.$bgcolor.';"></div>';
 		$recs[$i]['use%'].='</div>'.$pcntstr;	
 	}
 	$opts=array(
