f477dfe8869aee6d5523b57b306383964104bab7
Steven Lloyd <slloyd88@users.noreply.github.com>
2025-12-24 08:38:56 -0700

    adde XSS protection, removed misplaced git code.

--- php/admin/fonts_controller.php
+++ php/admin/fonts_controller.php
@@ -1,15 +1,8 @@
 <?php
-	global $git;
 	if(!isset($_REQUEST['func'])){$_REQUEST['func']='';}
 	switch(strtolower($_REQUEST['func'])){
-		case 'status':
-			$git['status']=gitCommand('status -s');
-			setView('git_status',1);
-			return;
-		break;
         default:
 			$fonts=fontsGetFonts();
-			//echo printValue($fonts);exit;
 			setView('default',1);
         break;
 	}
