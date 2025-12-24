661829b67aab461748ccf3cf317b20b458b86b50
Steven Lloyd <slloyd88@users.noreply.github.com>
2025-12-24 08:51:24 -0700

    added XSS protection

--- php/admin/ref_controller.php
+++ php/admin/ref_controller.php
@@ -1,9 +1,17 @@
 <?php
 global $path;
 $path=getWasqlPath('/php/temp');
+
+// Validate tab parameter - whitelist only
+$valid_tabs = array('html_tags', 'html_attributes', 'html_events', 'css_styles', 'css_selectors', 'css_functions', 'php_functions', 'sql_reference');
+$tab = 'html_tags'; // default
+
+if(isset($_REQUEST['tab']) && in_array($_REQUEST['tab'], $valid_tabs)){
+	$tab = $_REQUEST['tab'];
+}
+
 switch(strtolower($_REQUEST['func'])){
 	case 'list':
-		$tab=$_REQUEST['tab'];
 		setView('list',1);
 		return;
 	break;
