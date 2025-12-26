8ec12667f931995e7e5e9da1f7c945ccaf496cdd
Steven Lloyd <slloyd88@users.noreply.github.com>
2025-12-26 15:19:25 -0700

    Fixed XSS and SQL injection vulnerabilities

--- php/admin/zipcodes_controller.php
+++ php/admin/zipcodes_controller.php
@@ -6,10 +6,20 @@
 		$CONFIG['admin_menu_color']='w_gray';
 	}
 	setView('default');
-	if(isset($_REQUEST['country_codes'])){
-		if(!isDBTable('zipcodes')){
-			zipcodesCreateTable();
+	if(isset($_REQUEST['country_codes']) && is_array($_REQUEST['country_codes'])){
+		// Validate country codes - must be 2 letter codes
+		$validated_codes=array();
+		foreach($_REQUEST['country_codes'] as $code){
+			$code=preg_replace('/[^a-z]/is','',$code);
+			if(strlen($code)==2){
+				$validated_codes[]=$code;
+			}
 		}
-		$results=zipcodesImportCountry($_REQUEST['country_codes']);
+		if(count($validated_codes) > 0){
+			if(!isDBTable('zipcodes')){
+				zipcodesCreateTable();
+			}
+			$results=zipcodesImportCountry($validated_codes);
+		}
 	}
 ?>
